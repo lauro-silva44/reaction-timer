bf3a4c5c1eca906ecd04062291557fd4e106824f
lauro-silva44 <allvinhos@gmail.com>
2021-07-11 00:07:55 -0100

    added some hooks

--- src/components/Bloc.vue
+++ src/components/Bloc.vue
@@ -1,12 +1,33 @@
 <template>
-   <div class="block">
+   <div class="block" v-if="showBlock">
     click me   
     </div> 
+    
 </template>
 
 
 <script>
 export default {
+    props: ['delay'],
+    data(){
+        return {
+            showBlock: false
+        } 
+    
+    }, 
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true;
+            console.log(this.delay);
+        }, this.delay)
+    }, 
+    updated(){
+            console.log('component updated');
+    },
+    unmounted(){
+        console.log('component unmounted');
+    },
+
     name: '',
     components: {
 
